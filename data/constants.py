edc6100eff3171ef698b95eb88e717e6d9fb9a26
Luisa Chavarria <chavar18@msu.edu>
2025-03-31 09:41:32 -0400

    Add files via upload

---
+++ data/constants.py
@@ -0,0 +1,32 @@
+# constants.py
+"""""
+Module containing the constants required for the simulation.
+
+This module defines physical constants and ion radii that are used throughout the simulation. 
+
+The constants include: Boltzmann constant, Coulomb's constant, and the radii of various ions involved in the system.
+
+Author: Luisa Chavarria
+Date: March 2025
+
+"""
+
+# Constants for the simulation
+
+# Boltzmann constant in J/K
+k_B = 1.38e-23  
+
+# Coulomb's constant (in N·m²/C²)
+k_e = 8.99e9
+
+
+# Ion radii in picometers (pm)
+r_Mg = 72   # Magnesium (Mg 2+) 
+r_Fe = 78   # Iron (Fe 2+)
+r_Na = 102  # Sodium (Na +)
+r_O = 140   # Oxygen (O 2-)
+
+# Lattice size (dimension of the 3D grid)
+L = 5    # 5x5x5
+
+
